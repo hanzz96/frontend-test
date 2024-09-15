5e01f4b5c919f414b8c40f2abbb37a894c99a478
Johan <johanandreas74@gmail.com>
2024-09-16 01:19:06 +0700

    change dummy data warranty claim

--- src/_starter/warrantyClaimStarter.js
+++ src/_starter/warrantyClaimStarter.js
@@ -1,34 +1,38 @@
 export const warrantyClaimStarter = [
     {
-        id: 1,
+        _id: 1,
         product: {
-            id: 1,
+            _id: 1,
             name: 'Comverges',
             serialNumber: 'c5576a21-3bf6-4f25-80e4-99984bb7e2c6',
         },
         serialNumber: 'c5576a21-3bf6-4f25-80e4-99984bb7e2c6',
         status: 'pending',
         customer: {
-            id: 1,
+            _id: 1,
             name: 'Customer 1',
             role: 'CUSTOMER',
         },
-        createdAt: "2019-09-15T09:11:44 -07:00",
+        createdAt: "2019-09-15T09:11:44.543Z",
+        approvedAt: null,
+        approvedBy: null
     },
     {
-        id: 2,
+        _id: 2,
         product: {
-            id: 2,
+            _id: 2,
             name: 'Xsports',
             serialNumber: '814f7980-f033-4ac8-80c5-a598164d0b13',
         },
         serialNumber: '814f7980-f033-4ac8-80c5-a598164d0b13',
         status: 'pending',
         customer: {
-            id: 2,
+            _id: 2,
             name: 'Customer 2',
             role: 'CUSTOMER',
         },
-        createdAt: "2024-09-15T11:34:11 -07:00",
+        createdAt: "2024-09-15T11:34:11.543Z",
+        approvedAt: null,
+        approvedBy: null
     }
 ]   
