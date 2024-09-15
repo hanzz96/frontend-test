79ce16594667824a121eb0995ed83d2c1dd79ece
Johan <johanandreas74@gmail.com>
2024-09-16 00:30:51 +0700

    add description to remember

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  This is Home
+  Handling validation about data is assumed to be handled in the backend.
+  Since we are not integrate with backend, we assume that the data is valid.
 </template>
 
 <script>
